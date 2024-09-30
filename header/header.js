54cea6b8f21782110c87a7639aba033ae531a23c
ViraRiefertDev <vira.riefert.dev@gmail.com>
2024-09-30 22:42:56 +0200

    changes 3

--- header/header.js
+++ header/header.js
@@ -1,5 +1,5 @@
 //Загружаем header.html и вставляем его в div
-fetch('/header/header.html')
+fetch('header/header.html')
 .then(response => response.text())
 .then(data =>{
    document.getElementById('header-placeholder').innerHTML = data;
